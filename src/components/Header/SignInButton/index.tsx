f5928c871f0c54dcc11890bd09b041b768c829fd
WD-G.Kenji <g.kenjiJS@hotmail.com>
2021-06-07 16:15:06 -0300

    Feat: Autenticação com Next Auth

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,27 +1,32 @@
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client'
+
 import styles from './styles.module.scss';
 
 export function SignInButton(){
-    const isUserLoggedIn = true;
-    // const isUserLoggedIn = false;
+    const [ session ] = useSession();
 
-    return isUserLoggedIn ? ( // Logged
+    console.log(session);
+
+    return session ? ( // Logged
         <button 
             type="button"
             className={styles.singInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361"/>
-            Name User...
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : ( // Not logged in
         <button 
             type="button"
             className={styles.singInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417"/>
             Sign in with GitHub
         </button>
-    )
+    );
 }
