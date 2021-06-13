60406f6e9b3625a74f377101168c03284850da01
WD-G.Kenji <g.kenjiJS@hotmail.com>
2021-06-13 04:00:32 -0300

    Feat: Validando assinatura ativa

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,7 +1,9 @@
 import { signIn, useSession } from 'next-auth/client';
-import styles from './styles.module.scss';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
+
+import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
     priceId: string;
@@ -9,12 +11,18 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps){
     const [session] = useSession();
+    const router = useRouter();
 
     async function handleSubscribe(){
         if (!session) {
             signIn('github');
             return;
-        } 
+        }
+        
+        if (session.activeSubscription){
+            router.push('/posts');
+            return;
+        }
 
         try{
             const response = await api.post('/subscribe');
